61c37ee0bc39b01cf09c405a622c6905ca147741
Abhinash04 <abhinash.pritiraj@gmail.com>
2024-08-24 01:22:57 +0530

    seventh commit

--- src/components/home/Section1.js
+++ src/components/home/Section1.js
@@ -11,7 +11,7 @@ const Section1 = () => {
       <div className="fresnel-container fresnel-lessThan-desktop"></div>
       <div className="overflow-hidden">
         <div className="ContainerV2_1 relative z-20 mx-auto my-24 flex flex-nowrap gap-6 items-stretch h-max min-h-0 md:my-12">
-          <div className="min-w-[60%] flex-basis-[60%] flex-grow flex-shrink text-center md:text-left md:mr-0">
+          <div className="min-w-[60%] flex-basis-[60%] flex-grow flex-shrink text-left mt-5 md:text-left md:mr-0">
             <div className="box1 bannerV1">
               <div className="bannerV2">
                 <FontAwesomeIcon
@@ -39,35 +39,36 @@ const Section1 = () => {
                 </div>
               </div>
             </a>
-            <div className="hero-stats flex w-fit shadow-[0_0_50px_0_rgba(25,24,29,0.08)] rounded-lg bg-white items-center text-left max-w-full md:mx-auto md:w-fit sm:w-full">
-              <div className="StatItemStyledStatItem flex-[1_1_0%] w-1/2 flex m-[15px_0] px-[40px] sm:px-[24px] sm:m-[0_0_5px_-8px] sm:py-[9px]">
-                <div className="StatItemStatIconWrapper flex items-center mr-[20px]">
-                  <FontAwesomeIcon icon={faBriefcase} className="w-8 h-10 ml-20 text-[#ff9800]" />
-                </div>
-                <div className="StatItemStatIconWrapper whitespace-nowrap items-center">
-                  <h3 className="TV5 TV15">
-                    92%
-                  </h3>
-                  <p className="TV4 TV16">
-                    Job placement
-                  </p>
-                </div>
-              </div>
-              <div className="separator-line box1 flex h-[50px] w-0 opacity-50 border border-[#ececec]"></div>
-              <div className="StatItemStyledStatItem flex-[1_1_0%] w-1/2 flex m-[15px_0] px-[40px] sm:px-[24px] sm:m-[0_0_5px_-8px] sm:py-[9px]">
-                <div className="StatItemStatIconWrapper flex items-center mr-[20px]">
-                  <FontAwesomeIcon icon={faIndianRupeeSign} className="w-8 h-8 ml-20 text-[#ff9800]" />
-                </div>
-                <div className="StatItemStatIconWrapper whitespace-nowrap items-center">
-                  <h3 className="TV5 TV15">
-                    30 Lac
-                  </h3>
-                  <p className="TV4 TV16">
-                    Packages upto
-                  </p>
-                </div>
-              </div>
-            </div>
+            <div className="flex w-0 h-20 shadow-[0_0_50px_0_rgba(25,24,29,0.08)] rounded-lg bg-white items-center text-left md:mx-0 md:w-1/2 sm:w-1/2 ml-0">
+  <div className="StatItemStyledStatItem flex-[1_1_0%] w-1/2 flex m-[15px_0] px-[40px] sm:px-[24px] sm:m-[0_0_5px_-8px] sm:py-[9px] items-center justify-center">
+    <div className="StatItemStatIconWrapper flex items-center mr-[20px]">
+      <FontAwesomeIcon icon={faBriefcase} className="w-8 h-10 text-[#ff9800]" />
+    </div>
+    <div className="StatItemStatContent flex flex-col items-center">
+      <h3 className="text-lg font-bold leading-tight text-center">
+        92%
+      </h3>
+      <p className="text-sm leading-tight text-center">
+        Job placement
+      </p>
+    </div>
+  </div>
+  <div className="separator-line box1 flex h-[50px] w-0 opacity-50 border border-[#ececec]"></div>
+  <div className="StatItemStyledStatItem flex-[1_1_0%] w-1/2 flex m-[15px_0] px-[40px] sm:px-[24px] sm:m-[0_0_5px_-8px] sm:py-[9px] items-center justify-center">
+    <div className="StatItemStatIconWrapper flex items-center mr-[20px]">
+      <FontAwesomeIcon icon={faIndianRupeeSign} className="w-8 h-8 text-[#ff9800]" />
+    </div>
+    <div className="StatItemStatContent flex flex-col items-center">
+      <h3 className="text-lg font-bold leading-tight text-center">
+        30 Lac
+      </h3>
+      <p className="text-sm leading-tight text-center">
+        Packages upto
+      </p>
+    </div>
+  </div>
+</div>
+
           </div>
           {/* Update this div to include 'hidden' on smaller screens */}
           <div className="hero-image flex justify-center items-center lg:block hidden">
